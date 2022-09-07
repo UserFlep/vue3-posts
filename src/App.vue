e0d3f27078f4695f10b1b07d92c0ce0bb7f39a1e
UserFlep <era.user.ivan@gmail.com>
2022-09-07 13:06:29 +0300

    Реализован "вечный скролл" вместо постраничной пагинации

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="app">
+    //Поиск
     <my-input v-model="searchValue" placeholder="Поиск по названию"/>
+
+    //Кнопки
     <div class="app__btns">
       <my-button @click="showDialog">Создать пост</my-button>
       <my-select v-model="selectedSort" :options="sortOptions"/>
     </div>
 
+    //Форма создания поста
     <my-dialog v-model:show="dialogVisible">
       <post-form @create="createPost"/>
     </my-dialog>
 
-
+    //Спинер
     <my-spinner v-if="isPostLoading"/>
+
+    //Список постов
     <post-list v-else v-bind:posts="sortedAndSearchedPosts" @remove="removePost"/>
 
-    <my-page-wrapper v-show="!isPostLoading" :totalPages="totalPages" v-model="page"/>
+    //Наблюдаемый обьект для "вечного" скролла
+    <div class="observable" ref="observable"></div>
   </div>
 </template>
 <script>
@@ -40,7 +47,27 @@
       }
     },
     mounted() {
+      //Получаем первую партию постов
       this.fetchPosts()
+      //Подписываемся на попадания "observerable-а" в область видимости браузера для динамической подгрузки постов
+      //Опции
+      const options = {
+        rootMargin: '0px',
+        threshold: 1.0
+      }
+      //Колбек который выполнится при пересечении наблюдаемого обьекта
+      const callback = (entries, observer) => {
+        //Если обьект пересечен и текущая страница < общего количества страниц
+        //Получилось удобно, т.к. totalPages изначально = 0, а page=1, что блокирует срабатывание колбека
+        // при монтировании наблюдаемого обьекта
+        if(entries[0].isIntersecting && this.page < this.totalPages){
+          this.loadMorePosts()
+        }
+      };
+      const observer = new IntersectionObserver(callback, options);
+      //Ссылка а наблюдаемый обьект
+      const target = this.$refs.observable;
+      observer.observe(target);
     },
     methods: {
       createPost(post) {
@@ -53,11 +80,12 @@
       showDialog () {
         this.dialogVisible = true;
       },
-      changePage (pageNumber) {
-        this.page = pageNumber;
-      },
+      // changePage (pageNumber) {
+      //   this.page = pageNumber;
+      // },
       async fetchPosts () {
         try {
+          console.log("fetchPosts")
           this.isPostLoading = true;
           const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
             params: {
@@ -71,6 +99,22 @@
           alert(e)
         } finally {
           this.isPostLoading = false;
+        }
+      },
+      async loadMorePosts () {
+        try {
+          console.log("loadMorePosts")
+          this.page +=1;
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+            params: {
+              _page: this.page,
+              _limit: this.limit,
+            }
+          });
+          this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
+          this.posts = [...this.posts, ...response.data];
+        } catch (e){
+          alert(e)
         }
       }
     },
@@ -87,9 +131,9 @@
       }
     },
     watch: {
-      page(){
-        this.fetchPosts()
-      }
+      // page(){
+      //   this.fetchPosts()
+      // }
     }
   }
 </script>
@@ -106,5 +150,8 @@
     margin: 15px 0;
     display: flex;
     justify-content: space-between;
+  }
+  .observable {
+    height: 50px;
   }
 </style>
